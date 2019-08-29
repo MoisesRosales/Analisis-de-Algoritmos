ba0fe1a8d6423606495ba1d9930779cdd296ada7
MoisesRosales <00218916@uca.edu.sv>
2019-08-29 13:12:42 -0600

    Fix Ejem2

--- Laboratorio 1/Ejem2_cola.cpp
+++ Laboratorio 1/Ejem2_cola.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 // RECURCION POR COLA
+
 int fibo(int n){
     if(n == 0 || n == 1)
        return n;
     else
        return fibo(n - 2) + fibo(n - 1);
+}
+int Fibo_I(int n){
+    fibo(n);
+    return fibo(n);
 }
 int main(){
     cout<<"\n        FIBONACCI \n\n";
@@ -20,9 +25,9 @@ int main(){
     cout<<"\nLa serie es: \n\n\t";
     
     for(i=0; i<num; i++){
-       if(fibo(i) != 0)
+       if(Fibo_I(i) != 0)
           cout<< ",";
-          cout<< fibo(i);
+          cout<< Fibo_I(i);
     }
     return 0;
 }
